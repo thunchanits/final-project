c7126588fcf6918723a8ec32aed48c899538d65a
teemchaa <158262638+thunchanits@users.noreply.github.com>
2024-05-08 13:21:18 +0000

    icon home

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,10 @@ export default function Loginpage() {
     <div className="border text-lg bg-[url('/bglogin.png')] bg-cover min-h-[100vh] flex justify-center items-center">
       <div className="flex flex-col gap-4">
         <img className="w-[306px] h-[94px]" src="/logopasskit.svg"></img>
-        <h1 className="text-2xl font-normal text-white">Make your journey</h1>
-        <h2 className="text-2xl font-semibold text-white">more enjoyable.</h2>
+        <div className="flex flex-col">
+          <h1 className="text-2xl font-normal text-white">Make your journey</h1>
+          <h2 className="text-2xl font-semibold text-white">more enjoyable.</h2>
+        </div>
         <a
           href="/homepage"
           className=" bg-[#E8C6FF] text-black px-5 py-1 flex justify-center rounded-full max-w-fit"
--- app/passpage/page.tsx
+++ app/passpage/page.tsx
@@ -2,8 +2,8 @@ export default function passpage() {
   return (
     <div className="bg-[#070600] bg-cover min-h-[100vh] flex flex-col justify-center items-center gap-4 p-6">
       <div className="text-[#f2EFEA] flex flex-col items-center">
-        <h1 className="font-bold text-2xl italic">Journey</h1>
-        <h2 className="font-light text-2xl">Memories</h2>
+        <h1 className="font-bold text-3xl italic">Journey</h1>
+        <h2 className="font-light text-3xl">Memories</h2>
       </div>
       <div className="bg-[#f2EFEA] w-[375px] h-[620px] rounded-[40px] flex flex-col justify-between items-center gap-28 p-6">
         <div className="flex flex-col justify-center items-center gap-5">
@@ -12,12 +12,17 @@ export default function passpage() {
             <img className="size-[124px]" src="/stampfrance.svg"></img>
             <img className="size-[124px]" src="/stampjapan.svg"></img>
             <img className="size-[124px]" src="/stampitaly.svg"></img>
-            <img className="size-[124px]" src="/stampcanada.svg"></img>
+            <img
+              className="size-[124px] shadow-lg shadow-[#FF5500]"
+              src="/stampcanada.svg"
+            ></img>
           </div>
         </div>
         <h1 className="text-[#FF5500] font-normal text-base">1</h1>
       </div>
-      <img className="pt-10" src="/homeicon.svg"></img>
+      <a href="/collectpage">
+       <img className="pt-10" src="/homeicon.svg"></img>
+      </a>
     </div>
   );
 }
